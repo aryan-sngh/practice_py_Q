a90997e74bf259be54c732b1d214b6a48cd22ac1
Aryan Singh <arya.singh8930@gmail.com>
2025-01-11 23:07:57 +0530

    Update Answers.py

---
+++ Answers.py
@@ -0,0 +1,40 @@
+
+##1
+# a = int(input("enter the number"))
+# b = int(input("enter the 2nd number:"))
+# add = a + b
+# sub = a - b
+# multi = a * b
+# divide = a/b
+# print(add,sub,multi,divide)
+
+
+##2
+# a = int(input("enter any number:"))
+# b = int(input("enter 2nd number:"))
+# print(f"before swapping a = {a} and b = {b}")
+# temp = 0
+# temp = a
+# a = b
+# b = temp 
+# print(f"after swapping a = {a} and b = {b}")
+
+# #3
+# a = int(input("enter any number:"))
+# b = int(input("enter 2nd number:"))
+# print(f"before swap a = {a} and b = {b}")
+# a = a+b
+# b = a-b
+# a = a-b
+# print(f"after swap a = {a} and b = {b}")
+
+
+#4
+# a = int(input("enter the number: "))
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+    
+
+
